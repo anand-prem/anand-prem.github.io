c7940fe994b038bf6eaf8ee8e806c5fffe11c3a0
Anand Prem <anandprem333@gmail.com>
2017-03-11 21:09:04 +0530

    added image and title component

--- Resume/src/app/title/title.component.ts
+++ Resume/src/app/title/title.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'rsm-title',
-  templateUrl: './title.component.html'
+  templateUrl: './title.component.html',
+  styleUrls:['title.component.css']
 })
 export class TitleComponent {
-
+  myImagePath = "../favicon.ico";
 }
